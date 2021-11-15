6352790a1b44cea53a4e252787ce2978fc2788c2
Ahmad Syukron <ahmad05102001@gmail.com>
2021-11-15 23:00:31 +0700

    program

---
+++ Program_Python/Latihan1.py
@@ -0,0 +1,7 @@
+# Program mencari bilangan terbesar dari 2 buah bilangan
+a = int(input("Masukkan bilangan: "))
+b = int(input("Masukkan bilangan: "))
+if a > b:
+    print("A yang terbesar")
+else:
+    print("B yang terbesar")
---
+++ Program_Python/Latihan2.py
@@ -0,0 +1,8 @@
+#Program mengurutkan data dari yang terkecil
+print("Program mengurutkan data")
+a = int(input("Bilangan ke-1: "))
+b = int(input("Bilangan ke-2: "))
+c = int(input("Bilangan ke-3: "))
+num = [a, b, c]
+num.sort()
+print("Urutan bilangan: ", num)
